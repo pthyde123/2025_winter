8c15acb6d6a64a2f3dbfbc42715e03fc8e901147
pthyde123 <pth7@cornell.edu>
2024-09-13 14:01:49 -0400

    added script to generate csv for use on seed pack label printer

---
+++ scripts/winter_2025_seed_pack_labels.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(readr)
+
+
+winterOatPea_2025_plot_HR <- read_csv("output/winterOatPea_2025_plot_HR.csv")
+
+
+### making csv for use on Sorrels envelop printer
+### file must must have these headers in this order 
+###     source,plotNo,oatName,peaName
+### arrange by oatname so all plots for a given accession are together for seed separating from bulk bag
+
+
+
+winterOatPea_2025_plot_HR %>% 
+  mutate(peaName = if_else(crop_2=="oat-pea","Blaze","na")) %>% 
+  mutate(oatName = accession) %>% 
+  mutate(PlotNo = plotNo) %>% 
+  arrange(source,oatName) %>% 
+  select(source,plotNo,oatName,peaName) %>% 
+  write.csv( "output/winter_2025_envelopes.csv")
+
